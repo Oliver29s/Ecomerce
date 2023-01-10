505df62787264296f15c6cfa3084f6244dacc618
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-01-10 17:39:59 -0500

    seguimos correjir  errores

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+
+
+let contenCar = document.querySelector('.content_card')
+let iconCard = document.querySelector('.bxs-cart')
+
+iconCard.addEventListener('click',function(){
+    contenCar.classList.toggle('content_card_click')
+})
