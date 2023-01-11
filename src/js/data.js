3a3c5826cb30eeed7541fbd61275af23a1cf5898
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-01-11 11:09:35 -0500

    avanzando en la logica del carrito

---
+++ src/js/data.js
@@ -0,0 +1,26 @@
+export let products = [
+  {
+    id: 1,
+    image:'src/imagen/featured1.png',
+    name: "Hoddies",
+    category: "Hoddies",
+    price: 14.00,
+    stock: 10,
+  },
+  {
+    id: 2,
+    image: "src/imagen/featured3.png",
+    name: "Shirts",
+    category: "Shirts",
+    price: 24.00,
+    stock: 15,
+  },
+  {
+    id: 3,
+    image: 'src/imagen/home.png',
+    name: "Sweatshirts",
+    category: "Sweatshirts",
+    stock: 20,
+    price: 24.00,
+  },
+];
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,50 @@
+import { products } from "./data.js";
 
+console.log(products);
 
-let contenCar = document.querySelector('.content_card')
-let iconCard = document.querySelector('.bxs-cart')
+let contenCard = document.querySelector(".content_card");
+let iconCard = document.querySelector(".bxs-cart");
+let containCards = document.querySelector(".contain_cards");
+let objCarts = {}
 
-iconCard.addEventListener('click',function(){
-    contenCar.classList.toggle('content_card_click')
-})
+iconCard.addEventListener("click", function () {
+  contenCard.classList.toggle("content_card_click");
+});
+
+function prinProducts() {
+  let html = "";
+  products.forEach(({ id, name, image, category, stock, price }) => {
+    html += `
+                    <article class="card" >
+                        <div class="product_img">
+                            <img src="${image}" alt="${category}">
+                        </div>
+                        <div class="product_data" id="${id}">
+                            <h2>$${price}.00 <span>| Stock:${stock}</span></h2></h2>
+                            <h3>${name}</h3>
+                            <button><i class='bx bx-message-square-add bx-max'></i></button>
+                        </div>
+                    </article>      
+
+                 `;
+    containCards.innerHTML = html;
+  });
+}
+prinProducts();
+
+containCards.addEventListener("click", function (e) {
+  if (e.target.classList.contains("bx-max")) {
+    let idAll = Number(e.target.parentElement.parentElement.id);
+    let findCart = products.find(function (product) {
+      return product.id === idAll;
+    });
+    if (objCarts[idAll]) {
+        objCarts[idAll].amount++
+    }else{
+        objCarts[idAll] = {
+            ...findCart,
+            amount : 1
+        }
+    }
+  } console.log(objCarts);
+});
